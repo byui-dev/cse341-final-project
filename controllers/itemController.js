cca11f00ae71ca878437fbfd6c30ffa70c79addc
byui-dev <okdiphala@hotmail.com>
2026-06-06 11:06:53 +0200

    Refactor controllers/itemController.js

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,64 +1,49 @@
 const Item = require('../models/item');
+const { catchAsync } = require('../middleware/errorWithAsync');
 
 // Get all items
-exports.getItems = async (req, res) => {
-  try {  
+exports.getItems = catchAsync(async (req, res, next) => {
   const items = await Item.find();
   res.status(200).json(items);
-} catch (error) {
-  res.status(500).json({ message: error.message });
-}
-};
+});
 
 // Get single item
-exports.getItem = async (req, res) => {
-    try {
-        const item = await Item.findById(req.params.id);
-        if (!item) {
-            return res.status(404).json({ message: 'Item not found' });
-        }
-        res.status(200).json(item);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+exports.getItem = catchAsync(async (req, res, next) => {
+    const item = await Item.findById(req.params.id);
+    if (!item) {
+        res.status(404);
+        throw new Error("Item not found");
     }
-};  
+    res.status(200).json(item);
+});
 
-// Create new item
-exports.createItem = async (req, res) => {
-    try {
-        const item = await Item.create(req.body);
-        res.status(201).json(item);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-};
+ // Create new item
+exports.createItem = catchAsync(async (req, res, next) => {
+    // Note: The joi middleware will handle validation before this controller is called
+    const item = await Item.create(req.body);
+    res.status(201).json(item);
+});
 
 // Update item
-exports.updateItem = async (req, res) => {
-    try {
-        const item = await Item.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true, runValidators: true }
-        );
-        if (!item) {
-            return res.status(404).json({ message: 'Item not found' });
-        }
-        res.status(200).json(item);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
+exports.updateItem = catchAsync(async (req, res, next) => {
+    const item = await Item.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
+    if (!item) {
+        res.status(404);
+        throw new Error("Item not found");
     }
-};
+    res.status(200).json(item);
+});
 
 // Delete item
-exports.deleteItem = async (req, res) => {
-    try {
-        const item = await Item.findByIdAndDelete(req.params.id);
-        if (!item) {
-            return res.status(404).json({ message: 'Item not found' });
-        }
-        res.status(200).json({ message: 'Item deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+exports.deleteItem = catchAsync(async (req, res, next) => {
+    const item = await Item.findByIdAndDelete(req.params.id);
+    if (!item) {
+        res.status(404);
+        throw new Error("Item not found");
     }
-};
+    res.status(200).json({ message: 'Item deleted successfully' });
+});
+        
